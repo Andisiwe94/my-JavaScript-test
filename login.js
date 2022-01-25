798159451a05d201fe672030cdffae12e9a3d48e
unknown <andisiwegungu04@gmail.com>
2022-01-25 14:40:13 +0200

    alrtEmail added

--- login.js
+++ login.js
@@ -1,39 +1,35 @@
-// show a message with a type of the input
-function showMessage(exampleInputPassword1, exampleInputEmail1, type) {
-	
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+function ConfirmIdentity() {
+
+    var exampleInputEmail1;
+    
+ 
+    exampleInputEmail1 = (document.getElementById('exampleInputEmail1').value);
+   
+
+    if (exampleInputEmail1 == "") {
+        alert("email cannot be empty!!!");
+        return false;
+    } else {
+        return false;
+    }
+
+function ConfirmIdentity2() {
+
+    var exampleInputPassword1;
+
+    exampleInputPassword1 =(document.getElementById('password'));
+
+     if (exampleInputPassword1 == "") {
+        alert("password cannot be empty!!!");
+        return false;
+    } else {
+        return false;
+    }
 }
 
-function showError(exampleInputPassword1, exampleInputEmail1) {
-	return showMessage(exampleInputPassword1, exampleInputEmail1, false);
+   
+    
+
+    window.location.href = 'home.html'
 }
-
-function showSuccess(input) {
-	return showMessage(exampleInputPassword1, "", true);
-}
-
-function hasValue(exampleInputPassword1, exampleInputEmail1) {
-	if (input.value.trim() === "") {
-		return showError(exampleInputPassword1, exampleInputEmail1);
-	}
-	return showSuccess(input);
-}
-
-function validateEmail(inputexampleInputPassword1, requiredExampleInputEmail1, invalidExampleInputEmail1) {
-	// check if the value is not empty
-	if (!hasValue(exampleInputPassword1, requiredExampleInputEmail1)) {
-		return false;
-	}
-	// validate email format
-	const exampleInputEmail1Regex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	const exampleInputEmail1 = input.value.trim();
-	if (!exampleInputEmail1Regex.test(exampleInputEmail1)) {
-		return showError(exampleInputPassword1, invalidExampleInputEmail1);
-	}
-	return true;
-}  ;
 
