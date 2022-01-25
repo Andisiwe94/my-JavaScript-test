11f85a005f9575b4542825eb90e159c09506f494
unknown <andisiwegungu04@gmail.com>
2022-01-25 14:43:05 +0200

    function added

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
 function ConfirmIdentity() {
 
     var exampleInputEmail1;
+    var exampleInputPassword1;
     
  
     exampleInputEmail1 = (document.getElementById('exampleInputEmail1').value);
+    exampleInputPassword1 = (document.getElementById('exampleInputPassword1'));
    
 
     if (exampleInputEmail1 == "") {
@@ -13,19 +15,13 @@ function ConfirmIdentity() {
         return false;
     }
 
-function ConfirmIdentity2() {
-
-    var exampleInputPassword1;
-
-    exampleInputPassword1 =(document.getElementById('password'));
-
-     if (exampleInputPassword1 == "") {
+    if (exampleInputPassword1 == "") {
         alert("password cannot be empty!!!");
         return false;
     } else {
         return false;
     }
-}
+
 
    
     
