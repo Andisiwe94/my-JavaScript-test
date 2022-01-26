c2fae540cb46f947e471a6abf692a2482061c66d
unknown <andisiwegungu04@gmail.com>
2022-01-26 08:22:50 +0200

    alert password added

--- login.js
+++ login.js
@@ -1,31 +1,23 @@
+
+var exampleInputEmail1 = [];
+    var exampleInputPassword1 = [];
+
 function ConfirmIdentity() {
 
-    var exampleInputEmail1;
-    var exampleInputPassword1;
-    
- 
-    exampleInputEmail1 = (document.getElementById('exampleInputEmail1').value);
-    exampleInputPassword1 = (document.getElementById('exampleInputPassword1'));
+    exampleInputEmail1 = document.getElementById('exampleInputEmail1').value;
+    exampleInputPassword1 = document.getElementById('exampleInputPassword1').value;
    
 
     if (exampleInputEmail1 == "") {
         alert("email cannot be empty!!!");
-        return false;
-    } else {
-        return false;
+        return;
     }
 
     if (exampleInputPassword1 == "") {
         alert("password cannot be empty!!!");
-        return false;
-    } else {
-        return false;
-    }
-
-
-   
-    
-
-    window.location.href = 'home.html'
+        return;
+    } 
+    window.location.href = "home.html"
 }
+
 
